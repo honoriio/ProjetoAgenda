110d23f431ef7260c960370bcc1ab51bcca638dc
Diego Honório <diegohonoriiio@gmail.com>
2024-04-27 19:05:59 -0300

    Criando as funções colors e lines que serão usadas na interface do progrmama

---
+++ Src/Views/ColorsViews.py
@@ -0,0 +1,10 @@
+def Cores():
+    VERMELHO = '\033[91m'
+    VERDE = '\033[92m'
+    AMARELO = '\033[93m'
+    AZUL = '\033[94m'
+    MAGENTA = '\033[95m'
+    CIANO = '\033[96m'
+    BRANCO = '\033[97m'
+    RESET = '\033[0m'
+    return VERMELHO, VERDE, AMARELO, AZUL, MAGENTA, CIANO, BRANCO, RESET
---
+++ Src/Views/LinesViews.py
@@ -0,0 +1,13 @@
+def Lines():
+    linha1 =  '-' * 60
+    linha2 =  '_' * 60
+    linha3 =  '=' * 60
+    linha4 =  '*' * 60
+    linha5 =  '#' * 60
+    linha6 =  '@' * 60
+    linha7 =  '!' * 60
+    linha8 =  '"' * 60
+    linha9 =  "'" * 60
+    linha10 = '$' * 60
+
+    return linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8, linha9, linha10
