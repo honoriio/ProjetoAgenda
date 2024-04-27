87f1e294ade1be71a7509dc7444a54ec78608742
Diego Honório <diegohonoriiio@gmail.com>
2024-04-27 19:11:38 -0300

    Mudando os arquivos de pastas para deixar mais organizado

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+from Src.Views.Lines.LinesViews import Lines
+from Src.Views.Colors.ColorsViews import Cores
 
+linha1, linha2, linha3, linha4, linha5, linha6, linha7, linha8, linha9, linha10 = Lines()
+VERMELHO, VERDE, AMARELO, AZUL, MAGENTA, CIANO, BRANCO, RESET = Cores()
 
+print(AZUL + linha1 + RESET)
